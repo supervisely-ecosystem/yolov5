5aad4b16871017e75ce11545b60b451b71f87612
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-05-07 14:25:49 +0300

    pre-release for debug

--- supervisely/train/src/ui/hyperparameters.py
+++ supervisely/train/src/ui/hyperparameters.py
@@ -3,13 +3,13 @@ import supervisely.train.src.sly_train_globals as g
 
 
 def init(state):
-    state["epochs"] = 2  # 10 @TODO: for debug
+    state["epochs"] =  10
     state["batchSize"] = 16
     state["imgSize"] = 640
     state["multiScale"] = False
     state["singleClass"] = False
     state["device"] = '0'
-    state["workers"] = 0 #8  # 0 - for debug @TODO: for debug
+    state["workers"] = 8  # 0 - for debug @TODO: for debug
     state["activeTabName"] = "General"
     state["hyp"] = {
         "scratch": g.scratch_str,
