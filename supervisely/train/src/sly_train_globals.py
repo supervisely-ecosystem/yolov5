4bf4f34071a2bd07f328a6643fe55374c30ba6a6
TheoLisin <theo.lisin@gmail.com>
2023-11-02 11:41:04 +0000

    question

--- supervisely/train/src/sly_train_globals.py
+++ supervisely/train/src/sly_train_globals.py
@@ -27,6 +27,7 @@ if not sly.is_production():
     load_dotenv(os.path.join(root_source_dir, "supervisely", "train", "secret_debug.env"), override=True)
 
 my_app = AppService()
+# WHY?
 my_app._ignore_stop_for_debug = True
 
 team_id = int(os.environ['context.teamId'])
